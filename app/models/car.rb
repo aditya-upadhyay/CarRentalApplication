2a4a9d2cfc3e99af998f5519f640a2e0ea199f25
pjain15 <pjain15@ncsu.edu>
2017-09-22 21:46:22 -0400

    Car Validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,10 @@
 class Car < ApplicationRecord
-  validates_uniqueness_of :license_plate_number
+  validates :license_plate_number, uniqueness: true, numericality:true , presence: true , length: {is: 7}
+  validates :model, presence: true
+  validates :manufacturer, presence: true
+  validates :hourly_rental_rate, presence: true, numericality:true
+  validates :location, presence: true
+  validates :style , presence: true
+  validates :status ,presence: true, inclusion: { in: %w(Available Checkedout Reserved),
+                                                 message: "%{value} is not a valid status"}
 end
