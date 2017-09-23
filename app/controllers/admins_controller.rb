1eb8977aee48d003217740b2721f7f5125fcb7b4
pjain15 <pjain15@ncsu.edu>
2017-09-23 13:08:12 -0400

    AdminsController Uniqueness removed

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsController < ApplicationController
   before_action :set_admin, only: [:show, :edit, :update, :destroy]
-  validates :email, uniqueness: true
+
 
   # GET /admins
   # GET /admins.json
