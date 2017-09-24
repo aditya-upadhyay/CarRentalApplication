6c74d127e452e7345a6f09f40083d8dd94f0af7a
Vidhyalakshimi <vsreeni@ncsu.edu>
2017-09-24 19:56:06 -0400

    Fixed gitignored files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923012233) do
+ActiveRecord::Schema.define(version: 20170923211854) do
+
   create_table "admins", force: :cascade do |t|
     t.string "email"
     t.string "password"
