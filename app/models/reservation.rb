2d84dacab26ec37a291e3cdb1448eeb4cbcd7d02
pjain15 <pjain15@ncsu.edu>
2017-09-25 20:36:30 -0400

    Reservation Validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ApplicationRecord
+
+  validates :return_time, presence: true
+  validates :checkout_time, presence: true
+  validate :checkout_date_cannot_be_in_the_past
+  validate :seven_day_booking
+  validate :booking_span
+  validate :return_after_booking
+
+
   belongs_to :customer
   belongs_to :car
   def self.search(search_term)
@@ -7,4 +16,37 @@ class Reservation < ApplicationRecord
                   "%#{search_term}%"])
     end
   end
+
+  def checkout_date_cannot_be_in_the_past
+    if checkout_time.to_date< Date.today
+      errors.add(:checkout_time, "can't be in the past")
+    end
+  end
+  def seven_day_booking
+    difference_in_days = (checkout_time.to_date - Date.current).to_i
+    if difference_in_days>7
+      errors.add(:checkout_time, "Booking is allowed only 7 days in advance")
+    end
+  end
+  def booking_span
+    difference_in_days = (return_time - checkout_time).to_i
+    if difference_in_days==0
+      difference_in_time = (return_time.to_time-checkout_time.to_time).to_i
+      if difference_in_time >10
+       errors.add(:return_time, "- Car can be checkedout only for 10 hours")
+      else
+      if difference_in_time<1
+        errors.add(:return_time, "- Minimum reservation time is 1 hour")
+      end
+      end
+
+    else if difference_in_days*24>24
+           errors.add(:return_time, "-Maximum reservation of 10 hours is allowed")
+         end
+    end
+  end
+  def return_after_booking
+
+  end
+
 end
