cc08e7e4be062a83ac1c2f8baea33004f91ba39d
adupadhy <adupadhy@ncsu.edu>
2017-10-03 13:25:29 -0400

    Admin Authorizing

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927220535) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.string "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170923211854) do
 
   create_table "cars", force: :cascade do |t|
     t.string "license_plate_number"
@@ -48,8 +40,7 @@ ActiveRecord::Schema.define(version: 20170927220535) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.boolean "admin"
-    t.boolean "superadmin"
+    t.string "role_check", default: "customer"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
